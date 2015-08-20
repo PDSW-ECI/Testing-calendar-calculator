122df9100d5bc507cc5f4a2e9888c58aff77756e
Hector <hector.cadavid@gmail.com>
2015-08-20 13:32:21 -0500

    first revision

---
+++ src/test/java/edu/eci/pdsw/utils/test/TestCalendar.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.pdsw.utils.test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Conjunto de pruebas para XXXXXXXXX
+ * 
+
+Estados:
+
+                 +--------------------+      
++-+              |                    |      
++-+              |                    |      
+ |               |                    |      
+ |        +------v----+          +----+-----+
+ |        |           |          |          |
+ +-------->           +---------->          |
+          |           |          |          |
+          |           |          |          |
+          |           |          |          |
+          +--^--------+          +-------+--+
+             |                           |   
+             |                           |   
+             |                           |   
+             |        +-----------+      |   
+             |        |           |      |   
+             +--------+           |      |   
+                      |           <------+   
+                      |           |          
+                      |           |          
+                      +-----------+          
+
+Clases de equivalencia:
+
+1. Clase A
+2. Clase B
+3. Clase C
+
+Condiciones de frontera a evaluar:
+
+* @author XXXXXXX
+*/
+public class TestCalendar {
+    
+    public TestCalendar() {
+    }
+    
+    @Before
+    public void setUp() {
+        
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    /**
+     * @Objetivo
+     */
+    public void testAAAA(){
+       // fail("test not implemented yet.");
+    }
+
+    @Test
+    public void testBBBB(){
+       // fail("test not implemented yet.");
+    }
+
+    
+    
+}
